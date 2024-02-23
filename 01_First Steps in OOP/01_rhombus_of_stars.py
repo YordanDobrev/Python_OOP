a01ff71ccec7f87e61d9913c5e4b3ea733df18a8
YordanDobrev <petolisko@gmail.com>
2024-02-23 16:16:31 +0200

    Solved issue

--- 01_First Steps in OOP/01_rhombus_of_stars.py
+++ 01_First Steps in OOP/01_rhombus_of_stars.py
@@ -1,4 +1,19 @@
-a = 5
+size = int(input())
 
-print(a)
-#dffs
+
+def upper_part(upper_size):
+    for el in range(1, size):
+        print(f"{' ' * (size - el)}{'* ' * el}")
+
+
+def bottom_part(bottom_size):
+    for el in range(size, -1, -1):
+        print(f"{' ' * (size - el)}{'* ' * el}")
+
+
+def full_rhomb():
+    upper_part(size)
+    bottom_part(size)
+
+
+full_rhomb()
