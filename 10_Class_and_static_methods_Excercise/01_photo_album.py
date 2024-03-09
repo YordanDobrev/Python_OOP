f5213d0c993b510121fe0a5f35d26e5bab2d0d9e
YordanDobrev <petolisko@gmail.com>
2024-03-09 14:40:32 +0200

    Task resolved

---
+++ 10_Class_and_static_methods_Excercise/01_photo_album.py
@@ -0,0 +1,49 @@
+from math import ceil
+from typing import List
+
+
+class PhotoAlbum:
+    PHOTOS_PER_PAGE = 4
+    PAGE_SEPARATOR = 11
+    SYMBOL_SEPARATOR = "-"
+
+    def __init__(self, pages):
+        self.pages = pages
+        self.photos: List[List[str]] = [[] for _ in range(self.pages)]
+
+    @classmethod
+    def from_photos_count(cls, photos_count: int):
+        return cls(ceil(photos_count / cls.PHOTOS_PER_PAGE))
+
+    def add_photo(self, label):
+        for page in range(self.pages):
+            if len(self.photos[page]) < self.PHOTOS_PER_PAGE:
+                slot = len(self.photos[page]) + 1
+                self.photos[page].append(label)
+                return f"{label} photo added successfully on page {page + 1} slot {slot}"
+
+        return "No more free slots"
+
+    def display(self) -> str:
+        result = [
+            self.SYMBOL_SEPARATOR * self.PAGE_SEPARATOR,
+        ]
+
+        for page in self.photos:
+            result.append(("[] " * len(page)).rstrip())
+            result.append(self.SYMBOL_SEPARATOR * self.PAGE_SEPARATOR)
+
+        return "\n".join(result)
+
+
+album = PhotoAlbum(2)
+
+print(album.add_photo("baby"))
+print(album.add_photo("first grade"))
+print(album.add_photo("eight grade"))
+print(album.add_photo("party with friends"))
+print(album.photos)
+print(album.add_photo("prom"))
+print(album.add_photo("wedding"))
+
+print(album.display())
